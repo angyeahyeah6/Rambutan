8f3ff22335222e57b6b91d975d33432def1be19f
Wei Tzu Lee <weitzulee@WeinoMacBook-Pro.local>
2021-05-02 20:52:31 +0800

    a

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import { Button } from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.less';
-import '../test.less';
 Vue.config.productionTip = false
 Vue.use(Button);
 new Vue({
