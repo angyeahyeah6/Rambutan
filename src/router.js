bc5fa820bbb05cc71ff12196eccfb7488925f480
Wei Tzu Lee <weitzulee@WeinoMacBook-Pro.local>
2021-05-29 23:18:26 +0800

    changed router

--- src/router.js
+++ src/router.js
@@ -4,12 +4,14 @@ import Profile from "./components/Profile";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import OnlineMatch from './components/OnlineMatch'
+import ApplyInfo from './components/ApplyInfo';
 const routes = [
   { path: "/", redirect: "/Login" },
   { name: "Login", path: "/Login", component: Login },
   { name: "SignUp", path: "/SignUp", component: SignUp },
   { name: "Main", path: "/Main", component: Main },
   { name: "Info", path: "/Info/:id", component: Info },
+  { name: "ApplyInfo", path: "/applyInfo/:id", component: ApplyInfo },
   { name: "Profile", path: "/Profile", component: Profile },
   { name: "OnlineMatch", path: "/OnlineMatch", component: OnlineMatch },
 ];
